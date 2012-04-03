8e0953e12886a028cd583a4e52522a3f4d9e3347
thuvan <van01031990@gmail.com>
2012-04-03 16:52:04 +0700

    dang lam student_submit

---
+++ SPKTOnline/Controllers/NotbuildController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SPKTOnline.Controllers
+{
+    public class NotbuildController : Controller
+    {
+        //
+        // GET: /Notbuild/
+
+        public ActionResult Index(string view)
+        {
+            ViewBag.ViewName = view;
+            return View();
+        }
+
+    }
+}
---
+++ SPKTOnline/Controllers/Student_SubmitController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SPKTOnline.Models;
+using SPKTOnline.Management;
+
+namespace SPKTOnline.Controllers
+{
+    public class Student_SubmitController : Controller
+    {
+        //
+        // GET: /Student_Submit/
+        OnlineSPKTEntities1 db = new OnlineSPKTEntities1();
+        CheckRoles checkRole = new CheckRoles();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult TryTest(int ID)
+        {
+            if (User.Identity.IsAuthenticated == true)
+            {
+                if (checkRole.IsStudent(User.Identity.Name))
+                {
+                    Problem p = db.Problems.FirstOrDefault(m => m.ID == ID);
+                    Student_Summit st = new Student_Summit();
+                    st.Problem = p;
+                    return View(st);
+                }
+            }
+            return RedirectToAction("Logon", "Home");
+        }
+
+        [HttpPost]
+        public ActionResult TryTest(Student_Summit st)
+        {
+            if (User.Identity.IsAuthenticated == true)
+            {
+                if (checkRole.IsStudent(User.Identity.Name))
+                {
+                    st.StudentID = User.Identity.Name;
+                    st.TrangThaiBienDich = 1;
+                    st.TrangThaiCham = 1;
+                    st.LanguageID = 1;
+                    db.Student_Summit.AddObject(st);
+                    db.SaveChanges();
+                    return RedirectToAction("Index", "Home");//trả ra thông tin ở trang kết quả.
+                }
+            }
+            return RedirectToAction("Logon", "Home");
+        }
+
+    }
+}
