258a765b5ab0238e26b6d3450be405db1c79151e
ngocpq <phungquangngoc@gmail.com>
2012-05-08 22:23:52 +0700

    Add file Exceptions,

---
+++ ChamDiem/Exceptions/BienDichFileException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChamDiem.Exceptions
+{
+    public class BienDichFileException : ChamThiException
+    {
+        public BienDichFileException()
+            : base()
+        { }
+        public BienDichFileException(string message)
+            : base(message)
+        { }
+        public BienDichFileException(string message, Exception innerException)
+            : base(message, innerException)
+        { }
+    }
+}
---
+++ ChamDiem/Exceptions/RunFileException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChamDiem.Exceptions
+{
+    public class RunFileException:ChamThiException
+    {
+        public RunFileException()
+            : base()
+        { }
+        public RunFileException(string message)
+            : base(message)
+        { }
+        public RunFileException(string message, Exception innerException)
+            : base(message, innerException)
+        { }
+    }
+}
